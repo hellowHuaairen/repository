755ea439d46836de219ffe8b16c87cc1cd36e67c
zhangzuoqiang <ruyees@qq.com>
2013-04-19 20:29:47 +0800

    import

---
+++ deploy-files/deploy.bat
@@ -0,0 +1,10 @@
+@echo off
+echo [INFO] Deploy file to repository.
+
+call mvn deploy:deploy-file -DgroupId=com.ckfinder -DartifactId=apache-ant-zip -Dversion=2.3 -Dpackaging=jar -Dfile=files/apache-ant-zip.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=com.ckfinder -DartifactId=ckfinder -Dversion=2.3 -Dpackaging=jar -Dfile=files/CKFinder-2.3.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=com.ckfinder -DartifactId=ckfinderplugin-fileeditor -Dversion=2.3 -Dpackaging=jar -Dfile=files/CKFinderPlugin-FileEditor-2.3.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=com.ckfinder -DartifactId=ckfinderplugin-imageresize -Dversion=2.3 -Dpackaging=jar -Dfile=files/CKFinderPlugin-ImageResize-2.3.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=org.wltea -DartifactId=analyzer -Dversion=2012_u6 -Dpackaging=jar -Dfile=files/IKAnalyzer2012_u6.jar -Durl=file:../
+
+pause

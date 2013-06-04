e8768eb4ae8da7f087e12f30276d766be93b7f08
zhangzuoqiang <ruyees@qq.com>
2013-06-04 09:59:29 +0800

    update

--- deploy-files/deploy.bat
+++ deploy-files/deploy.bat
@@ -7,4 +7,9 @@ call mvn deploy:deploy-file -DgroupId=com.ckfinder -DartifactId=ckfinderplugin-f
 call mvn deploy:deploy-file -DgroupId=com.ckfinder -DartifactId=ckfinderplugin-imageresize -Dversion=2.3 -Dpackaging=jar -Dfile=files/CKFinderPlugin-ImageResize-2.3.jar -Durl=file:../
 call mvn deploy:deploy-file -DgroupId=org.wltea -DartifactId=analyzer -Dversion=2012_u6 -Dpackaging=jar -Dfile=files/IKAnalyzer2012_u6.jar -Durl=file:../
 
+call mvn deploy:deploy-file -DgroupId=org.htmlparser -DartifactId=htmlparser -Dversion=2.0 -Dpackaging=jar -Dfile=files/htmlparser.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=org.htmlparser -DartifactId=htmllexer -Dversion=2.0 -Dpackaging=jar -Dfile=files/htmllexer.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=com.webssky -DartifactId=jcseg -Dversion=1.8.2 -Dpackaging=jar -Dfile=files/jcseg-core-1.8.2.jar -Durl=file:../
+
 pause

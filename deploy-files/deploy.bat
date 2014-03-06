acd881a85a6cef47990892cd702c1196b0f3b195
faithstone <z.zuoqiang@gmail.com>
2014-03-06 09:56:19 +0800

    update

--- deploy-files/deploy.bat
+++ deploy-files/deploy.bat
@@ -12,4 +12,24 @@ call mvn deploy:deploy-file -DgroupId=org.htmlparser -DartifactId=htmllexer -Dve
 
 call mvn deploy:deploy-file -DgroupId=com.webssky -DartifactId=jcseg -Dversion=1.8.2 -Dpackaging=jar -Dfile=files/jcseg-core-1.8.2.jar -Durl=file:../
 
+call mvn deploy:deploy-file -DgroupId=org.tuckey -DartifactId=urlrewritefilter -Dversion=4.0.3 -Dpackaging=jar -Dfile=files/urlrewritefilter-4.0.3.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=zaric.rs -DartifactId=mgc -Dversion=0.0.1 -Dpackaging=jar -Dfile=files/mgc-0.0.1.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=zaric.rs -DartifactId=AbsoluteLayout -Dversion=0.0.1 -Dpackaging=jar -Dfile=files/AbsoluteLayout.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=zaric.rs -DartifactId=jxl -Dversion=0.0.1 -Dpackaging=jar -Dfile=files/jxl.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=zaric.rs -DartifactId=swing-layout -Dversion=1.0.3 -Dpackaging=jar -Dfile=files/swing-layout-1.0.3.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=ipcam -DartifactId=SmallSQL -Dversion=0.2.1 -Dpackaging=jar -Dfile=files/SmallSQL.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=ipcam -DartifactId=jgraphx -Dversion=2.2.0.2 -Dpackaging=jar -Dfile=files/jgraphx.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=ipcam -DartifactId=puremvc-java -Dversion=1.1 -Dpackaging=jar -Dfile=files/puremvc-java-1.1.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=ipcam -DartifactId=puremvc-java-multicore -Dversion=1.0.8 -Dpackaging=jar -Dfile=files/PureMVC_Java_MultiCore-1.0.8.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0 -Dpackaging=jar -Dfile=files/ojdbc6.jar -Durl=file:../
+
+call mvn deploy:deploy-file -DgroupId=com.artofsolving -DartifactId=jodconverter -Dversion=2.2.2 -Dpackaging=jar -Dfile=files/jodconverter-2.2.2.jar -Durl=file:../
+call mvn deploy:deploy-file -DgroupId=com.artofsolving -DartifactId=jodconverter-cli -Dversion=2.2.2 -Dpackaging=jar -Dfile=files/jodconverter-cli-2.2.2.jar -Durl=file:../
+
 pause
